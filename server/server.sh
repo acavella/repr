97ad433c6ac44306147aa0434adff77f7598e342
acavella <acavella@gmail.com>
2020-10-18 10:52:59 -0400

    add reposync

--- server/server.sh
+++ server/server.sh
@@ -25,3 +25,47 @@ __db="${__dir}/db"
 # Load variables from external config
 source ${__dir}/rs-server.conf
 
+# Color Table
+COL_NC='\e[0m' # No Color
+COL_LIGHT_GREEN='\e[1;32m'
+COL_LIGHT_RED='\e[1;31m'
+TICK="[${COL_LIGHT_GREEN}✓${COL_NC}]"
+CROSS="[${COL_LIGHT_RED}✗${COL_NC}]"
+INFO="[i]"
+# shellcheck disable=SC2034
+DONE="${COL_LIGHT_GREEN} done!${COL_NC}"
+OVER="\\r\\033[K"
+
+######## FUNCTIONS #########
+# All operations are built into individual functions for better readibility
+# and management.  
+
+is_command() {
+    # Checks for existence of string passed in as only function argument.
+    # Exit value of 0 when exists, 1 if not exists. Value is the result
+    # of the `command` shell built-in call.
+    local check_command="$1"
+
+    command -v "${check_command}" >/dev/null 2>&1
+}
+
+get_package_manager() {
+    # Check for common package managers per OS
+    if is_command dnf ; then
+        PKG_MGR="dnf" # set to dnf
+        SYSTEMD=1
+        printf "  %b Package manager: %s\\n" "${TICK}" "${PKG_MGR}"
+    elif is_command yum ; then
+        PKG_MGR="yum" # set to yum
+        SYSTEMD=0
+        printf "  %b Package manager: %s\\n" "${TICK}" "${PKG_MGR}"
+    else
+        # unable to detect a common yum based package manager
+        printf "  %b %bSupported package manager not found%b\\n" "${CROSS}" "${COL_LIGHT_RED}" "${COL_NC}"
+    fi
+}
+
+main () {
+    reposync -p ${SERVER_REPO} --gpg-check --repoid=${SRC_REPO}
+    exit 0 # clean exit
+}
